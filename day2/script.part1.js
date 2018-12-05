85bb61185c82235011460202ff39711e3d2656da
Raphael Hättich <11434@corp.competec.ch>
2018-12-05 10:04:14 +0100

    day two input and script

---
+++ day2/script.part1.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+ 
+const fileContent = fs.readFileSync('day2/input.txt', 'utf8');
+
+const ids = fileContent.split("\n")
+let twoLetters = 0;
+let threeLetters = 0;
+
+const calculateFrequency = (ids) => {
+    for (let id of ids) {
+        const map = lettersToMap(id);
+        let twoCount = 0;
+        let threeCount = 0;
+        map.forEach((count) => {
+            if(count === 2) {
+                twoCount++;
+            }
+            if(count === 3) {
+                threeCount++;
+            }
+        });
+        if(twoCount > 0) {
+            twoLetters++;
+        }
+        if(threeCount > 0) {
+            threeLetters++;
+        }
+    };
+};
+
+const lettersToMap = (id) => {
+    const map = new Map();
+    for (let letter of id) {
+        if (map.has(letter)) {
+            let currentValue = map.get(letter);
+            currentValue++;
+            map.set(letter, currentValue);
+        } else {
+            map.set(letter, 1);
+        }
+    }
+    return map;
+}
+
+calculateFrequency(ids);
+
+console.log('twoLetters: ', twoLetters);
+console.log('threeLetters: ', threeLetters);
+console.log('hash: ', twoLetters * threeLetters);
