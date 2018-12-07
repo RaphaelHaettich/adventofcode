274c3387f4348ce7f0cf86d9434dc0ea57a09794
Raphael Hättich <11434@corp.competec.ch>
2018-12-07 15:57:38 +0100

    part 1 day4 done

--- day4/findLongestSleeptime.day4.part1.js
+++ day4/findLongestSleeptime.day4.part1.js
@@ -1,53 +1,115 @@
 const fs = require('fs');
-const dayjs = require('dayjs');
 
-const fileContent = fs.readFileSync('day4/input.1.txt', 'utf8');
+const fileContent = fs.readFileSync('day4/input.txt', 'utf8');
 
 const logArray = fileContent.split("\n");
 
-
-
 const formatArray = (logArray) => {
-    // console.log(logArray);
-    const logArrayFormatted = [];
-    logArray.forEach(log => {
+    const logArrayFormatted = logArray.map(log => {
         const time = new Date(log.split('[')[1].split(']')[0]).getTime();
         const msg = log.split(']')[1].trim().split(' ');
-        logArrayFormatted.push({ time, msg });
+        return { time, msg };
     });
     return logArrayFormatted;
 };
-const sortArray = (logArrayFormatted) => logArrayFormatted.sort((a, b) => a.time - b.time);;
+const sortArray = (logArrayFormatted) => logArrayFormatted.sort((a, b) => a.time - b.time);
 
-const groupGuardActivites = (logArraySorted) => {
-    const groupedGuards = {};
-    let currentGuardObj = false;
-    for (let index = 0; index < logArraySorted.length; index++) {
-        const logArray = logArraySorted[index];
-        const action = logArray.msg[0] === 'wakes' ? 'start' : 'end';
-        if (logArray.msg[0] === 'Guard') {
-            if (currentGuardObj) {
-                groupedGuards[currentGuardObj.index].push({ time: logArray.time, action, guardId: currentGuardObj.guardId });
+const filterArray = (logArraySorted) => {
+    const uniqueGuardsArr = getUniqueGuards(logArraySorted);
+    const uniqueGuardObj = {};
+    uniqueGuardsArr.forEach(uniqueGuard => {
+        uniqueGuardObj[uniqueGuard] = [];
+        let addToArray = false;
+        uniqueGuardObj[uniqueGuard] = logArraySorted.filter((log) => {
+            if (log.msg[1] === uniqueGuard) {
+                addToArray = true;
+                return false;
+            } else if (log.msg[0] === 'Guard') {
+                addToArray = false;
+                return false;
             }
-            groupedGuards[index] = [{ time: logArray.time, action: 'start', guardId: logArray.msg[1] }];
-            currentGuardObj = { guardId: logArray.msg[1], index }
-            continue;
-        };
-        groupedGuards[currentGuardObj.index].push({ time: logArray.time, action, guardId: currentGuardObj.guardId });
+            if (addToArray) {
+                return true;
+            }
+        });
+    });
+    return uniqueGuardObj;
+};
+const getUniqueGuards = logArraySorted => [...new Set(logArraySorted.filter(log => log.msg[0] === 'Guard').map(log => log.msg[1]))];
+
+const getDuplicateTimesAndLength = (filteredArrays) => {
+    const results = [];
+    for (const key in filteredArrays) {
+        if (filteredArrays.hasOwnProperty(key)) {
+            results.push(getSleepTimesFunc(filteredArrays, key));
+        }
     }
-    return groupedGuards;
-};
 
-const calculateGuardSleepTime = (groupedGuardActivites) => {
+    return results;
+}
 
-};
+const getSleepTimesFunc = (filteredArrays, key) => {
+    const guardLog = filteredArrays[key];
+    const timeMap = new Map();
+    let sleepTime = 0;
+    const splitInCycle = guardLog.reduce(function (result, value, index, array) {
+        if (index % 2 === 0)
+            result.push(array.slice(index, index + 2));
+        return result;
+    }, []);
+    splitInCycle.forEach((cycle) => {
+        let date1 = new Date(cycle[0].time);
+        const date2 = new Date(cycle[1].time);
+        const difference = date2 - date1;
+        const differenceMinutes = difference / 1000 / 60;
+        sleepTime += difference;
+        for (let index = 0; index < differenceMinutes; index++) {
+            if (date1 !== date2) {
+                const minutes = date1.getMinutes();
+                date1 = new Date(date1.getTime() + 60000);
+                if (timeMap.has(minutes)) {
+                    timeMap.set(minutes, timeMap.get(minutes) + 1);
+                    continue;
+                }
+                timeMap.set(minutes, 1);
+            }
+        }
+    });
+    return { timeMap, sleepTime, key };
+}
+
+const getLongestSleepMinutes = (duplicateTimesAndLength) => {
+    let highestTimeObj = {
+        sleepTime: 0
+    };
+
+    duplicateTimesAndLength.forEach((elem) => {
+        if (elem.sleepTime > highestTimeObj.sleepTime) {
+            highestTimeObj = elem;
+        }
+    })
+
+    let highestSleepMinute = {time: 0};
+
+    for (var [key, value] of highestTimeObj.timeMap) {
+        if (value > highestSleepMinute.time) {
+            highestSleepMinute = {time: value, minute: key};
+        }
+    }
+
+    return { guardId: highestTimeObj.key, minuteMaxSleep: highestSleepMinute.minute };
+
+}
 
 const doFunc = (logArray) => {
     const logArrayFormatted = formatArray(logArray);
     const logArraySorted = sortArray(logArrayFormatted);
-    const groupedGuardActivites = groupGuardActivites(logArraySorted);
-    const calculatedGuardSleepTime = calculateGuardSleepTime(groupedGuardActivites);
-    console.log(calculatedGuardSleepTime);
+    const filteredArrays = filterArray(logArraySorted);
+    const duplicateTimesAndLength = getDuplicateTimesAndLength(filteredArrays);
+
+    const result = getLongestSleepMinutes(duplicateTimesAndLength);
+
+    console.log(`The guard ${result.guardId} was asleep the most at minute ${result.minuteMaxSleep}`);
 };
 
 doFunc(logArray);
