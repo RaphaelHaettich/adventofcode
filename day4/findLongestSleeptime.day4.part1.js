e35174a77e9ebcf96fd074b9c1f1eedc4a5ace28
Raphael Hättich <11434@corp.competec.ch>
2018-12-07 08:53:11 +0100

    day 4 p1 wip

--- day4/findLongestSleeptime.day4.part1.js
+++ day4/findLongestSleeptime.day4.part1.js
@@ -8,21 +8,42 @@ const logArray = fileContent.split("\n");
 
 
 const formatArray = (logArray) => {
-    console.log(logArray);
+    // console.log(logArray);
     const logArrayFormatted = [];
     logArray.forEach(log => {
         const time = new Date(log.split('[')[1].split(']')[0]).getTime();
-        const msg = log.split(']')[1].trim();
-        logArrayFormatted.push({time, msg});
+        const msg = log.split(']')[1].trim().split(' ');
+        logArrayFormatted.push({ time, msg });
     });
     return logArrayFormatted;
 };
-const sortArray = (logArrayFormatted) => logArrayFormatted.sort((a, b) =>  a.time - b.time);;
+const sortArray = (logArrayFormatted) => logArrayFormatted.sort((a, b) => a.time - b.time);;
+
+const groupGuardActivites = (logArraySorted) => {
+    const newObject = {};
+    let currentGuardObj = false;
+    for (let index = 0; index < logArraySorted.length; index++) {
+        console.log("here");
+        const logArray = logArraySorted[index];
+        const action = logArray.msg[0] === 'wakes' ? 'start' : 'end';
+        if (logArray.msg[0] === 'Guard') {
+            if (currentGuardObj) {
+                newObject[currentGuardObj.index].push({ time: logArray.time, action, guardId: currentGuardObj.guardId });
+            }
+            newObject[index] = [{ time: logArray.time, action: 'start', guardId: logArray.msg[1] }];
+            currentGuardObj = { guardId: logArray.msg[1], index }
+            continue;
+        };
+        newObject[currentGuardObj.index].push({ time: logArray.time, action, guardId: currentGuardObj.guardId });
+    }
+    return newObject;
+};
 
 const doFunc = (logArray) => {
     const logArrayFormatted = formatArray(logArray);
     const logArraySorted = sortArray(logArrayFormatted);
-    console.log(logArraySorted);
+    const groupedGuardActivites = groupGuardActivites(logArraySorted);
+    console.log(groupedGuardActivites);
 };
 
 doFunc(logArray);
